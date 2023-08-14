40b8584601fb43b6bc22608261f0cd04034ccc43
CarrotSea233 <2733614757@qq.com>
2023-08-14 11:13:06 +0800

    feat:初始化工程项目

---
+++ alinesno-infra-plat-console-domain/src/main/java/com/alinesno/infra/plat/console/mapper/package-info.java
@@ -0,0 +1,1 @@
+package com.alinesno.infra.plat.console.mapper;
---
+++ alinesno-infra-plat-console-facade/src/main/java/com/alinesno/infra/plat/console/service/AccountSiteService.java
@@ -0,0 +1,13 @@
+//package com.alinesno.infra.plat.console.service;
+//
+//import com.alinesno.infra.plat.console.entity.AccountSiteEntity;
+//import com.alinesno.infra.common.facade.services.IBaseService;
+//
+///**
+// * 用户站点Service接口
+// *
+// * @version 1.0.0
+// * @author luoxiaodong
+// */
+//public interface AccountSiteService extends IBaseService<AccountSiteEntity> {
+//}
