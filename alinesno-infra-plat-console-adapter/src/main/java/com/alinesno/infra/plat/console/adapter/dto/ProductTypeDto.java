eecab477d88f3dabece97a47ed4997a0d3de5dc1
周健(James) <jamesAip@gmail.com>
2024-08-26 18:50:00 -0400

    新增ProductTypeDto类，包含机构id、图标、名称等属性及方法。

---
+++ alinesno-infra-plat-console-adapter/src/main/java/com/alinesno/infra/plat/console/adapter/dto/ProductTypeDto.java
@@ -0,0 +1,25 @@
+package com.alinesno.infra.plat.console.adapter.dto;
+
+import com.alinesno.infra.common.facade.base.BaseDto;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.util.List;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+public class ProductTypeDto extends BaseDto {
+
+    private long orgId ; // 机构id
+    private String icon ; // 图标
+    private String name; // 名称
+    private String code ; // 业务类型编码
+    private String typeDescribe; // 产品类型描述
+    private int sortNumber; // 排序
+    private String banner; // 背景图
+    private String remark; // 备注
+    private String typeOwner ; // 类型所有者
+
+    private List<ProductItemDto> productItem;  // 产品项
+
+}
