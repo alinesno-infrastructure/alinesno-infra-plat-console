d09120206b56b7a106b96f5ab409fe70b9772766
周健(James) <jamesAip@gmail.com>
2024-08-27 05:02:00 -0400

    新增AppConfiguration类，实现CommandLineRunner接口用于项目配置。

---
+++ alinesno-infra-plat-console-boot/src/main/java/com/alinesno/infra/plat/console/config/AppConfiguration.java
@@ -0,0 +1,22 @@
+package com.alinesno.infra.plat.console.config;
+
+import com.alinesno.infra.common.web.adapter.sso.enable.EnableInfraSsoApi;
+import com.dtflys.forest.springboot.annotation.ForestScan;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 项目配置类
+ */
+@Slf4j
+@EnableInfraSsoApi
+@ForestScan(basePackages = "com.alinesno.infra.smart.assistant.adapter")
+@Configuration
+public class AppConfiguration implements CommandLineRunner {
+
+    @Override
+    public void run(String... args) throws Exception {
+        log.info("AppConfiguration.run {}", (Object) args);
+    }
+}
