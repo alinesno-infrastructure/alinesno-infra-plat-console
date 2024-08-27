92db4b9b88a1a09a704272332c88bdf532f7f83a
王明(AllenQin) <allenQin@gmail.com>
2024-08-26 20:25:00 -0400

    优化视图配置功能，添加上下排序及自定义名称设置

--- alinesno-infra-plat-console-ui/src/views/headTypeTab.vue
+++ alinesno-infra-plat-console-ui/src/views/headTypeTab.vue
@@ -31,10 +31,45 @@
       </div>
 
     </div>
+
+    <el-dialog v-model="dialogVisible" title="视图配置" width="500" :before-close="handleClose">
+
+      <!-- 实现 viewList 上下排序的配置 -->
+       <draggable v-model="menuList" :options="{ group: 'items', animation: 200 }" @end="onSortEnd">
+        <template #item="{ element, index }">
+          <el-card style="margin-bottom:5px;cursor: pointer;" shadow="hover" class="item-card">
+            <i :class="element.icon"></i>&nbsp;
+            <span v-if="!element.editing" @click="editName(index)">
+              {{ element.name }}
+            </span>
+            <el-input style="width: 80%;" size="large" v-else v-model="element.name" ref="inputRef" @blur="saveName(index)" @keyup.enter="saveName(index)" />
+          </el-card>
+        </template>
+      </draggable>
+
+      <span>双击可自定义名称.</span>
+
+      <template #footer>
+        <div class="dialog-footer">
+          <el-button @click="dialogVisible = false">取消</el-button>
+          <el-button type="primary" @click="handleConfirmViewList()">
+            确认 
+          </el-button>
+        </div>
+      </template>
+    </el-dialog>
+
   </div>
 </template>
 
 <script setup>
+
+import {
+  getViewList ,
+  updateViewList
+} from '@/api/console/dashboard';
+
+import draggable from "vuedraggable";
 
 const router = useRouter();
 const route = useRoute() //获取到值
@@ -48,16 +83,16 @@ const props = defineProps({
 
 let type = ref(-1);
 
-const dialogVisible = false;
+const dialogVisible = ref(false)
 
-const menuList = [
-  { status: 0, icon: 'fa-brands fa-slack', name: '仪盘表', type: '0', path: '/index', desc: '运营自动化门户' },
-  { status: 0, icon: 'fa-solid fa-pen-ruler', name: '研发服务', type: '1', path: '/dashboard/businessWorkspace', desc: '公共的业务建设组件服务' },
-  { status: 0, icon: 'fa-solid fa-rocket', name: '数据治理', type: '2', path: '/dashboard/dataWorkspace', desc: '数据治理开发治理' },
-  { status: 0, icon: 'fa-solid fa-sailboat', name: '智能服务', type: '3', path: '/dashboard/smartWorkspace', desc: '智能化专家服务' },
-  { status: 0, icon: 'fas fa-shipping-fast', name: '运维资产', type: '4', path: '/dashboard/operationWorkspace', desc: '整体服务的运营监控' },
-  { status: 0, icon: 'fas fa-feather fa-fw', name: '业务服务', type: '5', path: '/dashboard/customWorkspace', desc: '个性化服务视图配置' },
-];
+const menuList = ref([
+  // { status: 0, icon: 'fa-brands fa-slack', name: '仪盘表', type: '0', path: '/index', desc: '运营自动化门户' },
+  // { status: 0, icon: 'fa-solid fa-pen-ruler', name: '研发服务', type: '1', path: '/dashboard/businessWorkspace', desc: '公共的业务建设组件服务' },
+  // { status: 0, icon: 'fa-solid fa-rocket', name: '数据治理', type: '2', path: '/dashboard/dataWorkspace', desc: '数据治理开发治理' },
+  // { status: 0, icon: 'fa-solid fa-sailboat', name: '智能服务', type: '3', path: '/dashboard/smartWorkspace', desc: '智能化专家服务' },
+  // { status: 0, icon: 'fas fa-shipping-fast', name: '运维资产', type: '4', path: '/dashboard/operationWorkspace', desc: '整体服务的运营监控' },
+  // { status: 0, icon: 'fas fa-feather fa-fw', name: '业务服务', type: '5', path: '/dashboard/customWorkspace', desc: '个性化服务视图配置' },
+]);
 
 /** 选择类 */
 function addSelectClass(t) {
@@ -72,6 +107,43 @@ function addSelectClass(t) {
   return props.currentTab == t? 'select' : '';
 };
 
+/** 获取视图列表 */
+function handleViewList() {
+  getViewList().then(res => {
+    console.log(res);
+    menuList.value = res.data;
+  });
+}
+
+function editName(index) {
+  const item = menuList.value[index];
+  if (!item.editing) {
+    item.editing = true;
+    // 确保输入框获取焦点
+    nextTick(() => {
+      $refs.inputRef[index].focus();
+    });
+  }
+}
+
+function saveName(index) {
+  const item = menuList.value[index];
+  item.editing = false;
+}
+
+function onSortEnd(event) {
+  // 在这里可以处理排序结束后的逻辑
+}
+
+
+/** 确认视图配置 */
+function handleConfirmViewList(){
+  dialogVisible.value = false ;
+  updateViewList(menuList.value).then(res => {
+    console.log(res);
+  });
+}
+
 /** 路径跳转 */
 function jumpTo(item, index) {
   if (item.status == 1) {
@@ -83,5 +155,7 @@ function jumpTo(item, index) {
   }
   router.push({ name : item.path, params: { type: index } });
 }
+
+handleViewList() ;
 
 </script>
