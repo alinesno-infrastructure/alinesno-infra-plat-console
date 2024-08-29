28377d148170adeeb51f269e389651c1dd306884
李伟(Michael) <michaelAip@gmail.com>
2024-08-29 09:01:00 -0400

    fix:优化BasePlatformConsoleConsumer接口方法注释及参数名

--- alinesno-infra-plat-console-adapter/src/main/java/com/alinesno/infra/plat/console/adapter/BasePlatformConsoleConsumer.java
+++ alinesno-infra-plat-console-adapter/src/main/java/com/alinesno/infra/plat/console/adapter/BasePlatformConsoleConsumer.java
@@ -1,13 +1,8 @@
 package com.alinesno.infra.plat.console.adapter;
 
-import com.alinesno.infra.common.facade.response.AjaxResult;
 import com.alinesno.infra.common.facade.response.R;
 import com.alinesno.infra.plat.console.api.MenuItem;
-import com.dtflys.forest.annotation.BaseRequest;
-import com.dtflys.forest.annotation.Body;
-import com.dtflys.forest.annotation.Get;
-import com.dtflys.forest.annotation.Post;
-import org.apache.ibatis.annotations.Param;
+import com.dtflys.forest.annotation.*;
 
 import java.util.List;
 
@@ -24,30 +19,30 @@ public interface BasePlatformConsoleConsumer {
      * 打招呼信息
      */
     @Get("/api/infra/base/platform/provider/account/sayHello")
-    R<String> sayHello(@Param("accountId") long accountId);
+    R<String> getGreeting(@Query("accountId") long accountId);
 
     /**
      * 查询用户今天是否签到
      */
     @Get("/api/infra/base/platform/provider/account/isSignIn")
-    R<Boolean> isSignIn(@Param("accountId")  long accountId);
+    R<Boolean> isSignIn(@Query("accountId")  long accountId);
 
     /**
      * 用户签到
      */
     @Get("/api/infra/base/platform/provider/account/signIn")
-    R<Integer> signIn(@Param("accountId") long accountId);
+    R<Integer> signIn(@Query("accountId") long accountId);
 
     /**
      * 组织自定义视图查询
      */
     @Get("/api/infra/base/platform/provider/account/customView")
-    R<List<MenuItem>> customView(@Param("orgId") long orgId);
+    R<List<MenuItem>> customView(@Query("orgId") long orgId);
 
     /**
      * 保存组织自定义视图查询
      */
     @Post("/api/infra/base/platform/provider/account/saveCustomView")
-    R<Boolean> saveCustomView(@Body List<MenuItem> menuList);
+    R<Boolean> saveCustomView(@JSONBody List<MenuItem> menuList , @Query("orgId") long orgId);
 
 }
