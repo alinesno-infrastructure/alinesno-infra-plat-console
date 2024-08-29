8116c33b0446db90dca23f217b03cf37b75eb2a9
李伟(Michael) <michaelAip@gmail.com>
2024-08-29 04:07:00 -0400

    fix: 更新服务视图组件，优化服务展示内容与结构。

--- alinesno-infra-plat-console-ui/src/views/data-workspace/service.vue
+++ alinesno-infra-plat-console-ui/src/views/data-workspace/service.vue
@@ -13,7 +13,7 @@
                     <i class="fa-brands fa-dashcube"></i>
                   </div>
                 <div class="app-status-text-desc">
-                  展示当前运行服务概览和详情为最新功能预览版</div>
+                  展示当前运行服务概览预览版</div>
               </div>
             </div>
             <div class="acp-app-list">
@@ -24,8 +24,14 @@
                     <i :class="item.icon"></i>
                   </div>
                   <div class="app-info">
-                    <div class="app-item-title">{{ item.name }}</div>
-                    <div class="app-item desc">{{ item.desc }}</div>
+                    <div class="app-item-title">
+                      <el-link class="app-item-title" :underline="false" :href="item.linkPath" target="_blank">
+                        {{ item.name }}
+                      </el-link>
+                    </div>
+                    <div class="app-item desc">
+                      {{ truncateString(item.productDescribe , 15) }}
+                    </div>
                   </div>
                   <div class="app-tip" style="float: right;font-size: 0.8rem;color:#a5a5a5">
                     <i class="fas fa-tools"></i> <span class="counter issues-label">0</span>
@@ -52,16 +58,19 @@
 
 import RunStatus from './runStatus.vue'
 
-const apps = ref([
-  { icon: 'fa-solid fa-charging-station', name: '主数据服务', desc: 'With Route 53 (3 分钟)' },
-  { icon: 'fa-brands fa-slack', name: '数据上报服务', desc: 'With Route 53 (3 分钟)' },
-  { icon: 'fa-solid fa-ship', name: '数据集成服务', desc: 'With Route 53 (3 分钟)' },
-  { icon: 'fab fa-github-square', name: '数据编排计算服务', desc: 'With EC2 (2 分钟)' },
-  { icon: 'fa-solid fa-tags', name: '实时计算服务', desc: 'With Route 53 (3 分钟)' },
-  { icon: 'fa-solid fa-truck-fast', name: '数据安全服务', desc: 'With Route 53 (3 分钟)' },
-  { icon: 'fas fa-paper-plane', name: '数据总线服务', desc: 'With Route 53 (3 分钟)' },
-  { icon: 'fa-solid fa-hammer', name: '数据资产服务', desc: 'With Route 53 (3 分钟)' },
-  { icon: 'fas fa-feather fa-fw', name: '数据接口服务', desc: 'With Route 53 (3 分钟)' },
-]);
+import { getProductByTypeCode } from '@/api/console/product'
+
+const apps = ref([])
+
+function handleProductByTypeCode(){
+  getProductByTypeCode('alinesno-infra-data').then(response => {
+    console.log(response) ; 
+    apps.value = response.data ;
+  })
+}
+
+nextTick(() => {
+  handleProductByTypeCode();
+})
 
 </script>
