0a45dad0b31e45779e2fc31a217783f49aaa4f00
李伟(Michael) <michaelAip@gmail.com>
2024-08-29 00:47:00 -0400

    修复并优化service.vue页面内容展示，更新服务列表数据获取方式。

--- alinesno-infra-plat-console-ui/src/views/business-workspace/service.vue
+++ alinesno-infra-plat-console-ui/src/views/business-workspace/service.vue
@@ -13,19 +13,24 @@
                     <i class="fa-brands fa-dashcube"></i>
                   </div>
                 <div class="app-status-text-desc">
-                  展示当前运行服务概览和详情为最新功能预览版</div>
+                  展示当前运行服务概览预览版</div>
               </div>
             </div>
             <div class="acp-app-list">
               <ul>
                 <li class="app-items" style="width:calc(100% - 10px);padding:5px 0px" v-for="item in apps" :key="item">
                   <div class="app-icon" >
-                    <!-- <img :src="item.icon" /> -->
-                    <i :class="item.icon" style="font-size: 1rem"></i>
+                    <i :class="item.icon" style="font-size: 1.4rem"></i>
                   </div>
                   <div class="app-info">
-                    <div class="app-item-title">{{ item.name }}</div>
-                    <div class="app-item desc">{{ item.desc }}</div>
+                    <div class="app-item-title">
+                      <el-link class="app-item-title" :underline="false" :href="item.linkPath" target="_blank">
+                        {{ item.name }}
+                      </el-link>
+                    </div>
+                    <div class="app-item desc">
+                      {{ truncateString(item.productDescribe , 15) }}
+                    </div>
                   </div>
                   <div class="app-tip" style="float: right;font-size: 0.8rem;color:#a5a5a5">
                     <i class="fas fa-tools"></i> <span class="counter issues-label">0</span>
@@ -52,18 +57,19 @@
 
 import RunStatus from './runStatus.vue'
 
-const apps = ref([
-  {icon: 'fa-solid fa-charging-station' , name:'权限资源引擎服务' , desc:'With Route 53 (3 分钟)'},
-  {icon: 'fa-solid fa-truck' , name:'网关配置服务' , desc:'With Route 53 (3 分钟)'},
-  {icon: 'fa-solid fa-paper-plane' , name:'分布式配置中心服务' , desc:'With Route 53 (3 分钟)'},
-  {icon: 'fa-solid fa-ship' , name:'公共存储服务' , desc:'With EC2 (2 分钟)'},
-  {icon: 'fa-solid fa-tags' , name:'单点登陆管理服务' , desc:'With Route 53 (3 分钟)'},
-  {icon: 'fa-solid fa-file-export' , name:'安全验证码服务' , desc:'With Route 53 (3 分钟)'},
-  {icon: 'fa-solid fa-truck-fast' , name:'分布式消息服务' , desc:'With Route 53 (3 分钟)'},
-  {icon: 'fa-solid fa-ship' , name:'公共存储服务' , desc:'With EC2 (2 分钟)'},
-  {icon: 'fa-solid fa-ship' , name:'多渠道通知服务' , desc:'With Route 53 (3 分钟)'},
-  {icon: 'fa-solid fa-hammer' , name:'代码生成器服务' , desc:'With Route 53 (3 分钟)'},
-  {icon: 'fa-solid fa-truck' , name:'网关配置服务' , desc:'With Route 53 (3 分钟)'},
-]);
+import { getProductByTypeCode } from '@/api/console/product'
+
+const apps = ref([])
+
+function handleProductByTypeCode(){
+  getProductByTypeCode('alinesno-infra-base').then(response => {
+    console.log(response) ; 
+    apps.value = response.data ;
+  })
+}
+
+nextTick(() => {
+  handleProductByTypeCode();
+})
 
 </script>
