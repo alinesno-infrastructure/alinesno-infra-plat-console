bd608908f20adf2f08bc8982f2f00472cf3b89b3
李娜(Allen) <allenAip@gmail.com>
2024-08-29 19:25:00 -0400

    feat:添加产品类型保存接口及默认参数设置

--- alinesno-infra-plat-console-gateway/src/main/java/com/alinesno/infra/plat/console/api/controller/ProductController.java
+++ alinesno-infra-plat-console-gateway/src/main/java/com/alinesno/infra/plat/console/api/controller/ProductController.java
@@ -1,6 +1,7 @@
 package com.alinesno.infra.plat.console.api.controller;
 
 import cn.dev33.satoken.stp.StpUtil;
+import cn.hutool.core.util.IdUtil;
 import com.alinesno.infra.common.facade.response.AjaxResult;
 import com.alinesno.infra.common.web.adapter.login.account.CurrentAccountJwt;
 import com.alinesno.infra.plat.console.adapter.BasePlatformProductConsumer;
@@ -11,9 +12,7 @@ import com.alinesno.infra.plat.console.api.dto.SubProduct;
 import io.swagger.annotations.Authorization;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -38,6 +37,21 @@ public class ProductController {
     public AjaxResult getProductByTypeCode(String typeCode) {
         List<ProductItemDto> productItemDtoList = productConsumer.queryProductByTypeCode(typeCode).getData() ;
         return AjaxResult.success(productItemDtoList);
+    }
+
+    @PostMapping("/save")
+    public AjaxResult saveProductType(@RequestBody ProductItemDto productItemDto) {
+
+        long orgId = 123456L ;
+
+        productItemDto.setOrgId(orgId);
+        productItemDto.setProductOwner("org");
+        productItemDto.setProdStatus("internal");
+        productItemDto.setIdentityStatus("enabled");
+        productItemDto.setIsPublic(1);
+
+        String res = productConsumer.saveOrgProduct(productItemDto).getData() ;
+        return AjaxResult.success(res);
     }
 
 }
