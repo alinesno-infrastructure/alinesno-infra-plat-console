230e06979bc18ef2bd33e7e710a1bd9ea3ff91a7
赵阳(Yang) <yangAip@gmail.com>
2024-08-29 18:34:00 -0400

    修复并优化notices.vue文件的显示逻辑和样式。

--- alinesno-infra-plat-console-ui/src/views/dashboard/notices.vue
+++ alinesno-infra-plat-console-ui/src/views/dashboard/notices.vue
@@ -1,57 +1,6 @@
 <template>
   <div>
     <el-row class="acp-dashboard-panel" :gutter="20">
-      <el-col class="panel-col" :span="11">
-        <div class="grid-content">
-          <div class="panel-header">
-            <div class="header-title">运行优化建议</div>
-          </div>
-          <div class="panel-body" style="padding:0px;">
-            <el-empty v-if="tableData.length == 0" :image-size="100" description="这可能是因为您没有运行 Trusted Advisor 检查，或者您没有 Business 或 Enterprise Support 计划。" />
-
-            <el-table :data="tableData" stripe style="width: 100%">
-              <el-table-column type="index" width="55" label="序号" align="center" />
-              <el-table-column label="图标" align="center" width="55px" prop="icon">
-                  <template #default="scope">
-                    <span style="font-size:25px;color:#3b5998">
-                        <i class="fa-solid fa-file-word" />
-                    </span>
-                  </template>
-              </el-table-column>
-              <el-table-column prop="busType" label="业务类型" width="80" />
-              <el-table-column prop="projectName" label="应用名称" width="100" :show-overflow-tooltip="true" />
-              <el-table-column prop="content" label="优化建议" :show-overflow-tooltip="true" />
-              <el-table-column prop="level" align="center" label="建议级别" width="80">
-                <template #default="scope">
-                  <div class="role-icon">
-                    <span v-if="scope.row.level == 9">
-                      <el-button type="danger" bg link>
-                        <i class="fa-solid fa-bomb" style="font-size: 20px"></i>
-                      </el-button>
-                    </span>
-                    <span v-if="scope.row.level == 1">
-                      <el-button type="danger" bg link>
-                        <i class="fa-solid fa-fire" style="font-size: 20px"></i>
-                      </el-button>
-                    </span>
-                    <span v-if="scope.row.level == 2">
-                      <el-button type="danger" bg link>
-                        <i class="fa-solid fa-bug" style="font-size: 20px"></i>
-                      </el-button>
-                    </span>
-                    <span v-if="scope.row.level == 3">
-                      <el-button type="warning" bg link>
-                        <i class="fa-solid fa-triangle-exclamation" style="font-size: 20px"></i>
-                      </el-button>
-                    </span>
-                  </div>
-              </template>
-              </el-table-column>
-            </el-table>
-
-          </div>
-        </div>
-      </el-col>
       <el-col :span="6">
         <div class="grid-content">
           <div class="panel-header">
@@ -145,19 +94,54 @@
         </div>
       </el-col>
 
+      <el-col class="panel-col" :span="11">
+        <div class="grid-content">
+          <div class="panel-header">
+            <div class="header-title"><i class="fa-solid fa-user-tag"></i> 推荐频道</div>
+          </div>
+          <div class="panel-body" style="padding:0px;">
+            <el-empty v-if="tableData.length == 0" :image-size="100" description="这可能是因为您没有运行 Trusted Advisor 检查，或者您没有 Business 或 Enterprise Support 计划。" />
+
+            <el-table :data="tableData" stripe style="width: 100%">
+              <!-- <el-table-column type="index" width="55" label="序号" align="center" /> -->
+              <el-table-column label="角色" align="left" width="300px" prop="icon">
+                  <template #default="scope">
+                    <img :src="'http://data.linesno.com/icons/header/' + (scope.row.link+6) + '.jpg'" style="width:26px;height:26px;float:left; border-radius: 8px" />
+                    <div style="float: left;margin-top: 3px;margin-left: 10px;width: calc(100% - 50px);">
+                      {{ scope.row.name }} 
+                    </div>
+                  </template>
+              </el-table-column>
+              <el-table-column prop="count" label="业务类型" />
+              <el-table-column prop="level" align="center" label="建议级别" width="180">
+                <template #default="scope">
+                  <div class="role-icon">
+                    <el-button type="primary" bg link>
+                      <i class="fa-solid fa-user-tag" style="font-size: 15px;margin-right:5px"></i> 进入体验 
+                    </el-button>
+                  </div>
+              </template>
+              </el-table-column>
+            </el-table>
+
+          </div>
+        </div>
+      </el-col>
+
     </el-row>
 
   </div>
 </template>
 
 <script setup>
+
 const tableData = [
-  { inde2x: 1, busType: '在线支付', projectName: '支付宝', content: '优化支付流程，提升用户体验，包括简化支付步骤，增加支付方式选择，提高支付成功率', level: '9' },
-  { inde2x: 2, busType: '电商平台', projectName: '淘宝', content: '增加推荐算法，提高用户购买转化率，包括个性化推荐，热门商品推荐，购买引导等方面', level: '2' },
-  { inde2x: 5, busType: '出行服务', projectName: '滴滴打车', content: '提高司机端APP稳定性，减少崩溃情况，改善司机使用体验，优化内存管理和异常处理机制', level: '9' },
-  { inde2x: 6, busType: '餐饮外卖', projectName: '美团外卖', content: '优化订单配送算法，减少配送时间，提高用户满意度，优化骑手调度和路线规划', level: '2' },
-  { inde2x: 3, busType: '社交网络', projectName: '微信', content: '改进朋友圈算法，减少低质量内容展示，提高用户参与度和粘性，加强内容审核机制', level: '2' },
-  { inde2x: 7, busType: '社交媒体', projectName: '抖音', content: '改进内容推荐算法，增加用户粘性，提高用户留存率，优化视频推荐和用户关注推荐', level: '2' },
+  {name:'需求文档分析频道' , count:'正在分析数据工程结构，分析需求文档中', icon:'fa-solid fa-file-shield' , link:1} ,
+  {name:'项目功能细化分析频道' , count:'数据功能细化分析中，异常问题收集', icon:'fas fa-shipping-fast' , link:2} , 
+  {name:'K8S问题排查频道' , count:'排查k8s问题分析，正在发送给业务人员', icon:'fa-solid fa-feather-pointed' , link:3} , 
+  {name:'Ansible自动化工程师' , count:'生成自动化运营平台，正在集成业务服务', icon:'fas fa-train' , link:4} , 
+  {name:'产品客户服务频道' , count:'客户服务专家列表结构，正在跟客户沟通中', icon:'fas fa-server' , link: 5} , 
+  {name:'开发编码规范频道' , count:'编码结构的失败服务，正在进一步编码中', icon:'fas fa-pencil-ruler' , link: 6} ,
 ]
 
 </script>
