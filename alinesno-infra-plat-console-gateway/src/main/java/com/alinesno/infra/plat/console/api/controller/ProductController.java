e433fd1251bcc4139a65ea904dbdb1a1b2482f80
李娜(Allen) <allenAip@gmail.com>
2024-08-31 18:31:00 -0400

    fix:修复 ProductController.java 文件中相关逻辑错误

--- alinesno-infra-plat-console-gateway/src/main/java/com/alinesno/infra/plat/console/api/controller/ProductController.java
+++ alinesno-infra-plat-console-gateway/src/main/java/com/alinesno/infra/plat/console/api/controller/ProductController.java
@@ -1,21 +1,13 @@
 package com.alinesno.infra.plat.console.api.controller;
 
-import cn.dev33.satoken.stp.StpUtil;
-import cn.hutool.core.util.IdUtil;
 import com.alinesno.infra.common.facade.response.AjaxResult;
-import com.alinesno.infra.common.web.adapter.login.account.CurrentAccountJwt;
 import com.alinesno.infra.plat.console.adapter.BasePlatformProductConsumer;
 import com.alinesno.infra.plat.console.adapter.dto.ProductItemDto;
 import com.alinesno.infra.plat.console.adapter.dto.ProductTypeDto;
-import com.alinesno.infra.plat.console.api.dto.Product;
-import com.alinesno.infra.plat.console.api.dto.SubProduct;
-import io.swagger.annotations.Authorization;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 @Slf4j
