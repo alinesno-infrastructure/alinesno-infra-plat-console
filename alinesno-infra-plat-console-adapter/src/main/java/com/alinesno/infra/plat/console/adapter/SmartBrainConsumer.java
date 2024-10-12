c5589f8159e959b01172482c3dbb070f2a0fabf6
王磊(Levi) <leviAip@gmail.com>
2024-10-12 14:36:30 +0800

    feat:实现SmartBrainConsumer接口并添加获取所有Agent分类的方法。

--- alinesno-infra-plat-console-adapter/src/main/java/com/alinesno/infra/plat/console/adapter/SmartBrainConsumer.java
+++ alinesno-infra-plat-console-adapter/src/main/java/com/alinesno/infra/plat/console/adapter/SmartBrainConsumer.java
@@ -1,6 +1,12 @@
 package com.alinesno.infra.plat.console.adapter;
 
+import com.alinesno.infra.common.facade.response.R;
+import com.alinesno.infra.plat.console.adapter.dto.AgentChannelDto;
+import com.alinesno.infra.plat.console.adapter.dto.IndustryRoleCatalogDto;
 import com.dtflys.forest.annotation.BaseRequest;
+import com.dtflys.forest.annotation.Get;
+
+import java.util.List;
 
 /**
  * 平台消费者基类
@@ -10,5 +16,11 @@ import com.dtflys.forest.annotation.BaseRequest;
         connectTimeout = 30*1000
 )
 public interface SmartBrainConsumer {
+
+    /**
+     * 获取到所有Agent分类
+     */
+    @Get("/v1/api/infra/base/im/chat/getAllCatalog")
+    R<List<IndustryRoleCatalogDto>> getAllCatalog();
 
 }
