bbfc1314546f50b28d6fd1eb8e44f9734a883745
黄涛(Leo) <leoAip@gmail.com>
2024-12-25 00:26:01 +0800

    feat:在product-all.vue中添加对话框标题及内容更新

--- alinesno-infra-plat-console-ui/src/views/dashboard/product-all.vue
+++ alinesno-infra-plat-console-ui/src/views/dashboard/product-all.vue
@@ -2,6 +2,7 @@
     <div class="container">
       <el-dialog
         v-model="centerDialogVisible" 
+        title="AIP工作区全景图"
         width="50%" 
         :before-close="handleClose"
         :close-on-press-escape="false" 
@@ -9,7 +10,7 @@
 
         <div class="product-panel">
           <div class="product-text">
-            AIP工作区是专为构建、开发和管理智能体（Agents）而设计的综合性平台。它提供了一套完整的工具链，
+            工作区是专为构建、开发和管理智能体（Agents）而设计的综合性平台。它提供了一套完整的工具链，
             从数据收集与感知到智能决策和持续监控，助力打造智能体，以应对各种复杂的业务挑战。
           </div>
           <div class="product-img">
