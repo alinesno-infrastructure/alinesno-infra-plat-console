cca6a9449f9c1c12a4da0c383bf8f4e2cac04c2e
黄涛(Leo) <leoAip@gmail.com>
2025-01-21 22:07:35 +0800

    feat:在notices.vue中添加链接功能并优化显示内容

--- alinesno-infra-plat-console-ui/src/views/dashboard/notices.vue
+++ alinesno-infra-plat-console-ui/src/views/dashboard/notices.vue
@@ -13,7 +13,11 @@
                   <i :class="item.icon" style="font-size:25px;margin-right:5px;color: #3b5998;margin-top:10px;"></i>
                 </div>
                 <div class="item-text">
-                  <div class="item-text-main-title">{{ item.title }}</div>
+                  <div class="item-text-main-title">
+                    <a :href="item.link" target="_blank">
+                      {{ item.title }}
+                    </a>
+                  </div>
                   <div class="item-text-main-desc">{{ item.desc }}</div>
                 </div>
               </li>
@@ -29,16 +33,20 @@
           <div class="panel-header">
             <div class="header-title">最新解决方案</div>
           </div>
-          <div class="panel-body">
+          <div class="panel-body" v-loading="getNewSolutionLoading">
             <ul class="panel-item-text">
               <li v-for="item in solutionItems" :key="item">
                 <div class="item-datetime">
-                  <div class="item-datetime-month">7月</div>
-                  <div class="item-datetime-date">12</div>
+                  <div class="item-datetime-month">{{ handleDateFormat('m', item.addDate) }}</div>
+                  <div class="item-datetime-date">{{ handleDateFormat('d', item.addDate) }}</div>
                 </div>
                 <div class="item-text">
-                  <div class="item-text-main-title">{{ item.title }}</div>
-                  <div class="item-text-main-desc">{{ item.description }}</div>
+                  <div class="item-text-main-title">
+                    <a :href="item.link" target="_blank">
+                      {{ item.title }}
+                    </a>
+                  </div>
+                  <div class="item-text-main-desc">{{ truncateString(item.content,30) }}</div>
                 </div>
               </li>
             </ul>
@@ -86,18 +94,11 @@
 import {defineEmits} from 'vue'
 const emit = defineEmits(['handleChannelChat'])
 
-const chatTitle = ref("")
+import{ getNewSolution } from '@/api/console/dashboard'
+const getNewSolutionLoading = ref(true)
+
 const dialogVisible = ref(false)
 const roleChatUri = ref("")
-
-// const tableData = [
-//   {name:'需求文档分析频道' , count:'正在分析数据工程结构，分析需求文档中', icon:'fa-solid fa-file-shield' , link:1} ,
-//   {name:'项目功能细化分析频道' , count:'数据功能细化分析中，异常问题收集', icon:'fas fa-shipping-fast' , link:2} ,
-//   {name:'K8S问题排查频道' , count:'排查k8s问题分析，正在发送给业务人员，正在发送给业务人员', icon:'fa-solid fa-feather-pointed' , link:3} ,
-//   {name:'Ansible自动化工程师' , count:'生成自动化运营平台，正在集成业务服务', icon:'fas fa-train' , link:4} ,
-//   {name:'产品客户服务频道' , count:'客户服务专家列表结构，正在跟客户沟通中', icon:'fas fa-server' , link: 5} ,
-//   {name:'开发编码规范频道' , count:'编码结构的失败服务，正在进一步编码中', icon:'fas fa-pencil-ruler' , link: 6} ,
-// ]
 
 const acpProjectBuild = [
   {
@@ -105,43 +106,25 @@ const acpProjectBuild = [
     icon: 'fas fa-book-reader',
     title: '快速上手指南',
     desc: '完成你的第一个智能体创建与配置',
-    link: '/dashboard/getting-started/quick-start'
+    link: 'http://portal.infra.linesno.com/technical/brain/04_角色自动化.html'
   },
   {
     id: '3',
     icon: 'fas fa-code',
     title: '编写第一个智能体脚本',
     desc: '如何编写简单的脚本来实现自动化',
-    link: '/dashboard/getting-started/write-first-script'
+    link: 'http://portal.infra.linesno.com/technical/brain/01_自定义智能体开发流程.html '
   },
   {
     id: '4',
     icon: 'fas fa-wrench',
     title: '配置智能体知识库',
     desc: '如何设置智能体的数据资产和知识',
-    link: '/dashboard/getting-started/configure-permissions'
+    link: 'http://portal.infra.linesno.com/technical/brain/03_数据自动化采集.html '
   }
 ];
 
-const solutionItems = ref([
-  {
-    datetime: '2024-12-19',
-    title: '智能化运维分析：通过智能体优化系统性能',
-    description: '利用智能体实时监控和自动调整系统参数，降低了运维成本。'
-  },
-  {
-    datetime: '2024-12-19',
-    title: '智能化运维分析：智能日志管理与故障预测',
-    description: '引入智能体进行日志管理和故障预测，问题的提前预警和快速定位。'
-  },
-  {
-    datetime: '2024-12-19',
-    title: '软文推广营销：社交媒体自动化互动',
-    description: '使用智能体实现社交媒体上的自动化互动，增大曝光度和用户参与度。'
-  }
-]);
-
-
+const solutionItems = ref([]);
 const panelItems = ref([
   {
     icon: 'fa-solid fa-signature',
@@ -194,17 +177,41 @@ const panelItems = ref([
   }
 ]);
 
+// 格式化日期
+const handleDateFormat = (formatIdentifier, dateString) => {
+  // 创建一个新的Date对象
+  const date = new Date(dateString);
 
-/** 与单个频道发信息 */
-function handleChannelChat(item){
-  emit('handleChannelChat' , item)
-}
+  // 检查是否是有效的日期
+  if (isNaN(date.getTime())) {
+    throw new Error('Invalid date.');
+  }
 
-/** 与单个Role发信息 */
-function handleRoleChat(item){
-    roleChatUri.value = "http://alinesno-infra-smart-im-ui.beta.smart.infra.linesno.com/agentChat?roleId=1808349647059738625"
-    chatTitle.value = item.roleName ;
-    dialogVisible.value = true ;
-}
+  // 根据传入的标识符来决定返回月份还是日期
+  switch(formatIdentifier) {
+    case 'm':
+      // 返回带'月'字符的月份，并确保为两位数
+      return `${date.getMonth() + 1}`.padStart(2, '0') + '月';
+    case 'd':
+      // 返回两位数格式的日期
+      return date.getDate().toString().padStart(2, '0');
+    default:
+      throw new Error('Invalid format identifier. Use "m" for month or "d" for date.');
+  }
+};
+
+// 获取到最新的解决方案
+const handleGetNewSolution = () => {
+  getNewSolution().then((res) => {
+    getNewSolutionLoading.value = false
+    if (res.code == 200) {
+      solutionItems.value = res.data;
+    }
+  }).catch((error) => {
+    getNewSolutionLoading.value = false
+  }) ;
+};
+
+handleGetNewSolution();
 
 </script>
