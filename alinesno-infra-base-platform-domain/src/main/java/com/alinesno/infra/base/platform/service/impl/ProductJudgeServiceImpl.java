bc3752d1538090639374dbfef7549699e4467600
王磊(Levi) <leviAip@gmail.com>
2025-02-21 01:09:41 +0800

    feat:新增 ProductJudgeServiceImpl 类，实现产品判断服务

---
+++ alinesno-infra-base-platform-domain/src/main/java/com/alinesno/infra/base/platform/service/impl/ProductJudgeServiceImpl.java
@@ -0,0 +1,14 @@
+package com.alinesno.infra.base.platform.service.impl;
+
+import com.alinesno.infra.base.platform.entity.ProductJudgeEntity;
+import com.alinesno.infra.base.platform.mapper.ProductJudgeMapper;
+import com.alinesno.infra.base.platform.service.IProductJudgeService;
+import com.alinesno.infra.common.core.service.impl.IBaseServiceImpl;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class ProductJudgeServiceImpl extends IBaseServiceImpl<ProductJudgeEntity, ProductJudgeMapper> implements IProductJudgeService {
+
+}
