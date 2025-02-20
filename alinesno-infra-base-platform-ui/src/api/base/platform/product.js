f666bf1d5f4f5e62b4368b6bc4565e9d44ac66db
梁明树(Nina) <ninaAip@gmail.com>
2025-02-21 01:09:44 +0800

    feat:新增产品管理API模块

---
+++ alinesno-infra-base-platform-ui/src/api/base/platform/product.js
@@ -0,0 +1,91 @@
+import request from '@/utils/request'
+import { parseStrEmpty } from "@/utils/ruoyi";
+
+/**
+ * 数据库接口操作
+ * 
+ * @author luoxiaodong
+ * @since 1.0.0
+ */
+
+// 接口配置项
+var prefix = '/api/infra/base/platform/productItem/' ;
+var managerUrl = {
+    datatables : prefix +"datatables" ,
+    createUrl: prefix + 'add' ,
+    saveUrl: prefix + 'save' ,
+    updateUrl: prefix +"modify" ,
+    statusUrl: prefix +"changeStatus" ,
+    cleanUrl: prefix + "clean",
+    detailUrl: prefix +"detail",
+    removeUrl: prefix + "delete" ,
+    exportUrl: prefix + "exportExcel",
+    changeField: prefix + "changeField",
+    downloadFile: prefix + "downloadFile"
+}
+
+// 查询数据库列表
+export function listProductItem(query) {
+  return request({
+    url: managerUrl.datatables ,
+    method: 'post',
+    params: query
+  })
+}
+
+// 查询数据库详细
+export function getProductItem(databaseId) {
+  return request({
+    url: managerUrl.detailUrl + '/' + parseStrEmpty(databaseId),
+    method: 'get'
+  })
+}
+
+// 新增数据库
+export function addProductItem(data) {
+  return request({
+    url: managerUrl.saveUrl ,
+    method: 'post',
+    data: data 
+  })
+}
+
+// 修改数据库
+export function updateProductItem(data) {
+  return request({
+    url: managerUrl.updateUrl ,
+    method: 'put',
+    data: data
+  })
+}
+
+// 删除数据库
+export function delProductItem(databaseId) {
+  return request({
+    url: managerUrl.removeUrl + '/' + parseStrEmpty(databaseId),
+    method: 'delete'
+  })
+}
+
+
+// 状态【请填写功能名称】修改
+export function changeProductStatus(id , status) {
+    const data = {
+        id ,
+        status
+    }
+    return request({
+        url: managerUrl.statusUrl,
+        method: 'put',
+        params: query
+    })
+}
+
+// 修改字段
+export function changeProductField(data) {
+    return request({
+        url: managerUrl.changeField,
+        method: 'post',
+        data: data
+    })
+}
