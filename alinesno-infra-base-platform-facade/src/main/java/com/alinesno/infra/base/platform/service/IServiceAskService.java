22ab94a7b5edfcde106beef2ec71101304c7f410
王磊(Levi) <leviAip@gmail.com>
2025-02-21 01:09:44 +0800

    feat:新增 IServiceAskService 接口，定义服务层基础操作

---
+++ alinesno-infra-base-platform-facade/src/main/java/com/alinesno/infra/base/platform/service/IServiceAskService.java
@@ -0,0 +1,14 @@
+package com.alinesno.infra.base.platform.service;
+
+import com.alinesno.infra.base.platform.entity.PartnerEntity;
+import com.alinesno.infra.base.platform.entity.ServiceAskEntity;
+import com.alinesno.infra.common.facade.services.IBaseService;
+
+/**
+ *
+ *
+ * @author luoxiaodong
+ * @version 1.0.0
+ */
+public interface IServiceAskService extends IBaseService<ServiceAskEntity> {
+}
