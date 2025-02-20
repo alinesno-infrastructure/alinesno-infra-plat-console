d0346696be3a4e21c4b82a544be11b784228d778
王磊(Levi) <leviAip@gmail.com>
2025-02-21 01:09:43 +0800

    feat:新增 IProductJudgeService 接口，定义产品判断服务的基础操作。

---
+++ alinesno-infra-base-platform-facade/src/main/java/com/alinesno/infra/base/platform/service/IProductJudgeService.java
@@ -0,0 +1,13 @@
+package com.alinesno.infra.base.platform.service;
+
+import com.alinesno.infra.base.platform.entity.ProductJudgeEntity;
+import com.alinesno.infra.common.facade.services.IBaseService;
+
+/**
+ *
+ *
+ * @author luoxiaodong
+ * @version 1.0.0
+ */
+public interface IProductJudgeService extends IBaseService<ProductJudgeEntity> {
+}
