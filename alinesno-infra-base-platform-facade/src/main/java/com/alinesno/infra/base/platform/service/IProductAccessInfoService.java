633ad0958d1aef3858f56c7676537a83607a688c
王磊(Levi) <leviAip@gmail.com>
2025-02-21 01:09:41 +0800

    feat:新增 IProductAccessInfoService 接口

---
+++ alinesno-infra-base-platform-facade/src/main/java/com/alinesno/infra/base/platform/service/IProductAccessInfoService.java
@@ -0,0 +1,13 @@
+package com.alinesno.infra.base.platform.service;
+
+import com.alinesno.infra.base.platform.entity.ProductAccessInfoEntity;
+import com.alinesno.infra.common.facade.services.IBaseService;
+
+/**
+ *
+ *
+ * @author luoxiaodong
+ * @version 1.0.0
+ */
+public interface IProductAccessInfoService extends IBaseService<ProductAccessInfoEntity> {
+}
