9826d2b7daf8a25e2fc90f08ad9c19fa474585e6
王磊(Levi) <leviAip@gmail.com>
2025-02-21 01:09:42 +0800

    feat:新增 IProductAccountService 接口

---
+++ alinesno-infra-base-platform-facade/src/main/java/com/alinesno/infra/base/platform/service/IProductAccountService.java
@@ -0,0 +1,14 @@
+package com.alinesno.infra.base.platform.service;
+
+import com.alinesno.infra.base.platform.entity.AccountAuditEntity;
+import com.alinesno.infra.base.platform.entity.ProductAccountEntity;
+import com.alinesno.infra.common.facade.services.IBaseService;
+
+/**
+ *
+ *
+ * @author luoxiaodong
+ * @version 1.0.0
+ */
+public interface IProductAccountService extends IBaseService<ProductAccountEntity> {
+}
