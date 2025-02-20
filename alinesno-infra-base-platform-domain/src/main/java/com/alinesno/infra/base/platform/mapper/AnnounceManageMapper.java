e658acc6efc52bc9ed56ae1693ccaed5f17586be
王磊(Levi) <leviAip@gmail.com>
2025-02-21 01:09:42 +0800

    feat:新增 AnnounceManageMapper 接口

---
+++ alinesno-infra-base-platform-domain/src/main/java/com/alinesno/infra/base/platform/mapper/AnnounceManageMapper.java
@@ -0,0 +1,16 @@
+package com.alinesno.infra.base.platform.mapper;
+
+import com.alinesno.infra.base.platform.entity.AnnounceManageEntity;
+import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
+
+/**
+ * <p>
+ * 持久层接口
+ * </p>
+ *
+ * @author LuoXiaoDong
+ * @version 1.0.0
+ */
+public interface AnnounceManageMapper extends IBaseMapper<AnnounceManageEntity> {
+
+}
