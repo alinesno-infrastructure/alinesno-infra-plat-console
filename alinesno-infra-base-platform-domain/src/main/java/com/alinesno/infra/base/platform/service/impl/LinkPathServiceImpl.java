440bdbdb1cb4adc7d769d03a723b1f72a0ae226f
王磊(Levi) <leviAip@gmail.com>
2025-02-21 01:09:42 +0800

    feat:新增 LinkPathServiceImpl 类，实现链接路径服务接口

---
+++ alinesno-infra-base-platform-domain/src/main/java/com/alinesno/infra/base/platform/service/impl/LinkPathServiceImpl.java
@@ -0,0 +1,14 @@
+package com.alinesno.infra.base.platform.service.impl;
+
+import com.alinesno.infra.base.platform.entity.LinkPathEntity;
+import com.alinesno.infra.base.platform.mapper.LinkPathMapper;
+import com.alinesno.infra.base.platform.service.ILinkPathService;
+import com.alinesno.infra.common.core.service.impl.IBaseServiceImpl;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class LinkPathServiceImpl extends IBaseServiceImpl<LinkPathEntity, LinkPathMapper> implements ILinkPathService {
+
+}
