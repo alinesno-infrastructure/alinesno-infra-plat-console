6bb524f192f3bdff83de23df3fa5b97359f20f8c
王磊(Levi) <leviAip@gmail.com>
2025-02-21 01:09:43 +0800

    feat:新增 AppScreenProductItemServiceImpl 类，实现相关业务逻辑。

---
+++ alinesno-infra-base-platform-domain/src/main/java/com/alinesno/infra/base/platform/service/impl/AppScreenProductItemServiceImpl.java
@@ -0,0 +1,14 @@
+package com.alinesno.infra.base.platform.service.impl;
+
+import com.alinesno.infra.base.platform.entity.AppScreenProductItemEntity;
+import com.alinesno.infra.base.platform.mapper.AppScreenProductItemMapper;
+import com.alinesno.infra.base.platform.service.IAppScreenProductItemService;
+import com.alinesno.infra.common.core.service.impl.IBaseServiceImpl;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class AppScreenProductItemServiceImpl extends IBaseServiceImpl<AppScreenProductItemEntity, AppScreenProductItemMapper> implements IAppScreenProductItemService {
+
+}
