03afc9d6d277d094edb022be82c13df807a1a090
王磊(Levi) <leviAip@gmail.com>
2025-02-21 01:09:42 +0800

    feat:新增 SolutionLabelServiceImpl 类，实现解决方案标签服务

---
+++ alinesno-infra-base-platform-domain/src/main/java/com/alinesno/infra/base/platform/service/impl/SolutionLabelServiceImpl.java
@@ -0,0 +1,14 @@
+package com.alinesno.infra.base.platform.service.impl;
+
+import com.alinesno.infra.base.platform.entity.SolutionLabelEntity;
+import com.alinesno.infra.base.platform.mapper.SolutionLabelMapper;
+import com.alinesno.infra.base.platform.service.ISolutionLabelService;
+import com.alinesno.infra.common.core.service.impl.IBaseServiceImpl;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class SolutionLabelServiceImpl extends IBaseServiceImpl<SolutionLabelEntity, SolutionLabelMapper> implements ISolutionLabelService {
+
+}
