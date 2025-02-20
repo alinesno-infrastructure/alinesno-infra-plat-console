fc9aef4c77f52536c26d62e11f81acd08b63faac
王磊(Levi) <leviAip@gmail.com>
2025-02-21 01:09:44 +0800

    feat:新增 ServiceAskMapper 接口，定义持久层操作

---
+++ alinesno-infra-base-platform-domain/src/main/java/com/alinesno/infra/base/platform/mapper/ServiceAskMapper.java
@@ -0,0 +1,17 @@
+package com.alinesno.infra.base.platform.mapper;
+
+import com.alinesno.infra.base.platform.entity.AccountAuditEntity;
+import com.alinesno.infra.base.platform.entity.ServiceAskEntity;
+import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
+
+/**
+ * <p>
+ * 持久层接口
+ * </p>
+ *
+ * @author LuoXiaoDong
+ * @version 1.0.0
+ */
+public interface ServiceAskMapper extends IBaseMapper<ServiceAskEntity> {
+
+}
