77236011496743140990844deb19fc1d2f518107
王磊(Levi) <leviAip@gmail.com>
2025-02-21 01:09:40 +0800

    feat:新增 IAgentChannelService 接口，定义代理通道服务的基础操作。

---
+++ alinesno-infra-base-platform-facade/src/main/java/com/alinesno/infra/base/platform/service/IAgentChannelService.java
@@ -0,0 +1,13 @@
+package com.alinesno.infra.base.platform.service;
+
+import com.alinesno.infra.base.platform.entity.AgentChannelEntity;
+import com.alinesno.infra.common.facade.services.IBaseService;
+
+/**
+ *
+ *
+ * @author luoxiaodong
+ * @version 1.0.0
+ */
+public interface IAgentChannelService extends IBaseService<AgentChannelEntity> {
+}
