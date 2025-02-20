e64c10deea5a422888483914481da0a8dc74d919
王磊(Levi) <leviAip@gmail.com>
2025-02-21 01:09:39 +0800

    feat:新增 AnnounceManageServiceImpl 类实现公告管理服务

---
+++ alinesno-infra-base-platform-domain/src/main/java/com/alinesno/infra/base/platform/service/impl/AnnounceManageServiceImpl.java
@@ -0,0 +1,14 @@
+package com.alinesno.infra.base.platform.service.impl;
+
+import com.alinesno.infra.base.platform.entity.AnnounceManageEntity;
+import com.alinesno.infra.base.platform.mapper.AnnounceManageMapper;
+import com.alinesno.infra.base.platform.service.IAnnounceManageService;
+import com.alinesno.infra.common.core.service.impl.IBaseServiceImpl;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class AnnounceManageServiceImpl extends IBaseServiceImpl<AnnounceManageEntity, AnnounceManageMapper> implements IAnnounceManageService {
+
+}
