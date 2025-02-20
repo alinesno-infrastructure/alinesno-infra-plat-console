d8f165d4a88b72e38fe8fbcb28903324f5e58249
王磊(Levi) <leviAip@gmail.com>
2025-02-21 01:09:39 +0800

    feat:实现 `ProductSolutionServiceImpl` 类，提供产品解决方案服务的基础实现。

---
+++ alinesno-infra-base-platform-domain/src/main/java/com/alinesno/infra/base/platform/service/impl/ProductSolutionServiceImpl.java
@@ -0,0 +1,14 @@
+package com.alinesno.infra.base.platform.service.impl;
+
+import com.alinesno.infra.base.platform.entity.ProductSolutionEntity;
+import com.alinesno.infra.base.platform.mapper.ProductSolutionMapper;
+import com.alinesno.infra.base.platform.service.IProductSolutionService;
+import com.alinesno.infra.common.core.service.impl.IBaseServiceImpl;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class ProductSolutionServiceImpl extends IBaseServiceImpl<ProductSolutionEntity, ProductSolutionMapper> implements IProductSolutionService {
+
+}
