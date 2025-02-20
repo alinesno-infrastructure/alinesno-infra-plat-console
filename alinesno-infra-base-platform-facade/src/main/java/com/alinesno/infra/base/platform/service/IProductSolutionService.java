c8c7aff483f6f4f3e8cd7fd5cf5dbb18e86c657a
王磊(Levi) <leviAip@gmail.com>
2025-02-21 01:09:43 +0800

    feat:新增 IProductSolutionService 接口

---
+++ alinesno-infra-base-platform-facade/src/main/java/com/alinesno/infra/base/platform/service/IProductSolutionService.java
@@ -0,0 +1,14 @@
+package com.alinesno.infra.base.platform.service;
+
+
+import com.alinesno.infra.base.platform.entity.ProductSolutionEntity;
+import com.alinesno.infra.common.facade.services.IBaseService;
+
+/**
+ *
+ *
+ * @author luoxiaodong
+ * @version 1.0.0
+ */
+public interface IProductSolutionService extends IBaseService<ProductSolutionEntity> {
+}
