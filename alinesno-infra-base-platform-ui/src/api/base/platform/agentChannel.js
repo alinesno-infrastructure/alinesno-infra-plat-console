f1dee2b0de41453da83d48695306cbfd512e0896
梁明树(Nina) <ninaAip@gmail.com>
2025-02-21 01:09:42 +0800

    feat:新增 agentChannel 接口操作模块

---
+++ alinesno-infra-base-platform-ui/src/api/base/platform/agentChannel.js
@@ -0,0 +1,68 @@
+import request from '@/utils/request'
+import { parseStrEmpty } from "@/utils/ruoyi";
+
+/**
+ * 数据库接口操作
+ * 
+ * @author luoxiaodong
+ * @since 1.0.0
+ */
+
+// 接口配置项
+var prefix = '/api/infra/base/platform/agentChannel/' ;
+var managerUrl = {
+    datatables : prefix +"datatables" ,
+    createUrl: prefix + 'add' ,
+    saveUrl: prefix + 'save' ,
+    updateUrl: prefix +"modify" ,
+    statusUrl: prefix +"changeStatus" ,
+    cleanUrl: prefix + "clean",
+    detailUrl: prefix +"detail",
+    removeUrl: prefix + "delete" ,
+    exportUrl: prefix + "exportExcel",
+    changeField: prefix + "changeField",
+    downloadFile: prefix + "downloadFile"
+}
+
+// 查询数据库列表
+export function listAgentChannel(query) {
+  return request({
+    url: managerUrl.datatables ,
+    method: 'post',
+    params: query
+  })
+}
+
+// 查询数据库详细
+export function getAgentChannel(databaseId) {
+  return request({
+    url: managerUrl.detailUrl + '/' + parseStrEmpty(databaseId),
+    method: 'get'
+  })
+}
+
+// 新增数据库
+export function addAgentChannel(data) {
+  return request({
+    url: managerUrl.saveUrl ,
+    method: 'post',
+    params: query
+  })
+}
+
+// 修改数据库
+export function updateAgentChannel(data) {
+  return request({
+    url: managerUrl.updateUrl ,
+    method: 'put',
+    params: query
+  })
+}
+
+// 删除数据库
+export function delAgentChannel(databaseId) {
+  return request({
+    url: managerUrl.removeUrl + '/' + parseStrEmpty(databaseId),
+    method: 'delete'
+  })
+}
