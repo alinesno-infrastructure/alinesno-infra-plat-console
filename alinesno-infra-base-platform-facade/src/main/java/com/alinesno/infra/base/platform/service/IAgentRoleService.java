f932960422b595cdceb48e887c29227edcad978c
王磊(Levi) <leviAip@gmail.com>
2025-02-21 01:09:44 +0800

    feat:新增 IAgentRoleService 接口，定义代理角色服务的基础操作

---
+++ alinesno-infra-base-platform-facade/src/main/java/com/alinesno/infra/base/platform/service/IAgentRoleService.java
@@ -0,0 +1,13 @@
+package com.alinesno.infra.base.platform.service;
+
+import com.alinesno.infra.base.platform.entity.AgentRoleEntity;
+import com.alinesno.infra.common.facade.services.IBaseService;
+
+/**
+ *
+ *
+ * @author luoxiaodong
+ * @version 1.0.0
+ */
+public interface IAgentRoleService extends IBaseService<AgentRoleEntity> {
+}
