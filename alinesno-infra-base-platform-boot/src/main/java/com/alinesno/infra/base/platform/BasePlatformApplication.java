ce6673f4ad474ccc930b657e4e98a8fc06aa75d1
王磊(Levi) <leviAip@gmail.com>
2025-02-21 01:09:42 +0800

    feat:新增BasePlatformApplication启动类

---
+++ alinesno-infra-base-platform-boot/src/main/java/com/alinesno/infra/base/platform/BasePlatformApplication.java
@@ -0,0 +1,18 @@
+package com.alinesno.infra.base.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 集成一个Java开发示例工具
+ * @author luoxiaodong
+ * @version 1.0.0.
+ */
+@SpringBootApplication
+public class BasePlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasePlatformApplication.class, args);
+	}
+
+}
