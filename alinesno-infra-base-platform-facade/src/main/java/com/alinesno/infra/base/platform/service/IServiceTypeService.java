7452c332756abe58b70d9350e171bfd51880ceac
王磊(Levi) <leviAip@gmail.com>
2025-02-21 01:09:39 +0800

    feat:新增 IServiceTypeService 接口，定义服务类型管理的基本操作。

---
+++ alinesno-infra-base-platform-facade/src/main/java/com/alinesno/infra/base/platform/service/IServiceTypeService.java
@@ -0,0 +1,14 @@
+package com.alinesno.infra.base.platform.service;
+
+import com.alinesno.infra.base.platform.entity.ServiceAskEntity;
+import com.alinesno.infra.base.platform.entity.ServiceTypeEntity;
+import com.alinesno.infra.common.facade.services.IBaseService;
+
+/**
+ *
+ *
+ * @author luoxiaodong
+ * @version 1.0.0
+ */
+public interface IServiceTypeService extends IBaseService<ServiceTypeEntity> {
+}
