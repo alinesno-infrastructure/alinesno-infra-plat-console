ba3800fc66a1988aa6e38d5a30e9455da186b6e6
王磊(Levi) <leviAip@gmail.com>
2025-02-21 01:09:41 +0800

    feat:新增 ILinkPathService 接口，定义基础服务方法

---
+++ alinesno-infra-base-platform-facade/src/main/java/com/alinesno/infra/base/platform/service/ILinkPathService.java
@@ -0,0 +1,14 @@
+package com.alinesno.infra.base.platform.service;
+
+import com.alinesno.infra.base.platform.entity.AccountAuditEntity;
+import com.alinesno.infra.base.platform.entity.LinkPathEntity;
+import com.alinesno.infra.common.facade.services.IBaseService;
+
+/**
+ *
+ *
+ * @author luoxiaodong
+ * @version 1.0.0
+ */
+public interface ILinkPathService extends IBaseService<LinkPathEntity> {
+}
