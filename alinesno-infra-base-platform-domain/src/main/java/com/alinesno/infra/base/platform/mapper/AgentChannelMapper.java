5505802450ecec7dda6f94c300a91fee540e0b23
王磊(Levi) <leviAip@gmail.com>
2025-02-21 01:09:39 +0800

    feat:新增 AgentChannelMapper 接口，定义持久层操作

---
+++ alinesno-infra-base-platform-domain/src/main/java/com/alinesno/infra/base/platform/mapper/AgentChannelMapper.java
@@ -0,0 +1,16 @@
+package com.alinesno.infra.base.platform.mapper;
+
+import com.alinesno.infra.base.platform.entity.AgentChannelEntity;
+import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
+
+/**
+ * <p>
+ * 持久层接口
+ * </p>
+ *
+ * @author LuoXiaoDong
+ * @version 1.0.0
+ */
+public interface AgentChannelMapper extends IBaseMapper<AgentChannelEntity> {
+
+}
