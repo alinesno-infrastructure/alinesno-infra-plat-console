d20a74df82f8b57a9180c0738819bb6c63eabc50
王磊(Levi) <leviAip@gmail.com>
2025-02-21 01:09:43 +0800

    feat:新增 IAppScreenService 接口，定义应用屏幕管理服务的基础操作

---
+++ alinesno-infra-base-platform-facade/src/main/java/com/alinesno/infra/base/platform/service/IAppScreenService.java
@@ -0,0 +1,14 @@
+package com.alinesno.infra.base.platform.service;
+
+import com.alinesno.infra.base.platform.entity.AccountAuditEntity;
+import com.alinesno.infra.base.platform.entity.AppScreenEntity;
+import com.alinesno.infra.common.facade.services.IBaseService;
+
+/**
+ *
+ *
+ * @author luoxiaodong
+ * @version 1.0.0
+ */
+public interface IAppScreenService extends IBaseService<AppScreenEntity> {
+}
