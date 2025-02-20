f0771354194d351ae6f192a8159d28ee619e8564
王明(AllenQin) <allenQin@gmail.com>
2025-02-21 01:09:39 +0800

    feat:新增首页视图组件index.vue

---
+++ alinesno-infra-base-platform-ui/src/views/index.vue
@@ -0,0 +1,132 @@
+<template>
+  <div class="app-container acp-dashboard">
+
+    <el-row>
+      <el-col :span="24">
+        <div class="aip-appinfo-header">
+          <div class="header-icon-banner">
+            <i class="fa-solid fa-computer"></i>
+          </div>
+          <div class="icon">
+            <div class="title">
+                 {{ currentEnvClusterObj.appName}}
+              <div class="cluster-info">
+                <span>
+                  <i class="el-icon-monitor"></i>
+                  {{ currentEnvClusterObj.clusterName }}
+                  </span>
+              </div>
+            </div>
+            <div class="title-desc">
+                  {{ currentEnvClusterObj.desc }}
+            </div>
+          </div>
+        </div>
+      </el-col>
+    </el-row>
+
+    <!-- 应用统计 -->
+    <!-- <OperationWorkspaceCountStaitcs /> -->
+
+    <!-- 应用套件和状态 -->
+    <OperationWorkspaceService />
+
+    <!-- 运行应用列表 -->
+    <OperationWorkspaceApps />
+
+  </div>
+</template>
+
+<script setup name="Index">
+
+// import OperationWorkspaceCountStaitcs from './operation-workspace/countStatics.vue'
+
+import OperationWorkspaceApps from './operation-workspace/apps.vue'
+import OperationWorkspaceService from './operation-workspace/service.vue'
+
+const currentEnvClusterObj = ref({
+  appName: 'AIP平台运营管理服务',
+  clusterName: '统一的平台管理和运营，用于统一平台管理'  ,
+  apiServerUrl: 'http://portal.infra.linesno.com' ,
+  desc: 'AIP平台运营管理服务，用于平台的产品的管理和运营服务'
+})
+
+</script>
+
+
+<style lang="scss" scoped>
+.aip-appinfo-header{
+  position: relative;
+  padding: 20px;
+  overflow: hidden;
+
+  .header-icon-banner {
+    float: left;
+    font-size: 2.4rem;
+    margin-right: 10px;
+    color: #3b5998;
+  }
+
+  .head-app-status{
+    float: right;
+    font-size: 14px;
+    line-height: 1.7rem;
+    margin-bottom: 10px;
+
+    .integrated-item-box {
+
+      margin-left: 10px;
+      float: right;
+
+      ul , li {
+        margin: 0px;
+        padding: 0px;
+        list-style: none;
+      }
+      li {
+        float:right ;
+        margin-right: 10px;
+
+        img {
+          border-radius: 5px;
+          width: 25px;
+        }
+      }
+
+    }
+  }
+
+  .cluster-info {
+    float: right;
+    position: relative;
+    font-size: 14px;
+    margin-left: 10px;
+    font-weight: bold;
+    margin-top: 10px;
+    color: #3b5998;
+
+    span {
+      margin-left: 20px;
+    }
+  }
+
+  .icon {
+    float: left;
+  }
+
+  .title {
+    display: block;
+    font-weight: 600;
+    font-style: normal;
+    font-size: 24px;
+    color: #242e42;
+    text-shadow: 0 4px 8px rgba(36, 46, 66, 0.1);
+    margin-bottom: 10px;
+  }
+
+  .title-desc {
+    color: #79879c !important;
+    font-size: 12px;
+  }
+}
+</style>
