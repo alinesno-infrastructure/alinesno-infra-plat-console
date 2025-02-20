cd63a1c17cef9ce4f3d72aa8da61d0d8ffe65fd1
王磊(Levi) <leviAip@gmail.com>
2025-02-21 01:09:39 +0800

    feat:新增 AccountAuditMapper 接口，实现持久层操作

---
+++ alinesno-infra-base-platform-domain/src/main/java/com/alinesno/infra/base/platform/mapper/AccountAuditMapper.java
@@ -0,0 +1,18 @@
+package com.alinesno.infra.base.platform.mapper;
+
+import com.alinesno.infra.base.platform.entity.AccountAuditEntity;
+import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
+
+import java.util.List;
+
+/**
+ * <p>
+ * 持久层接口
+ * </p>
+ *
+ * @author LuoXiaoDong
+ * @version 1.0.0
+ */
+public interface AccountAuditMapper extends IBaseMapper<AccountAuditEntity> {
+
+}
