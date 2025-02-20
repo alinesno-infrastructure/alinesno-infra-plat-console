aca3188ad1b207af6627550ca313472fc54288dc
王磊(Levi) <leviAip@gmail.com>
2025-02-21 01:09:42 +0800

    feat:新增 IAnnounceManageService 接口，定义公告管理服务的基本操作。

---
+++ alinesno-infra-base-platform-facade/src/main/java/com/alinesno/infra/base/platform/service/IAnnounceManageService.java
@@ -0,0 +1,13 @@
+package com.alinesno.infra.base.platform.service;
+
+import com.alinesno.infra.base.platform.entity.AnnounceManageEntity;
+import com.alinesno.infra.common.facade.services.IBaseService;
+
+/**
+ *
+ *
+ * @author luoxiaodong
+ * @version 1.0.0
+ */
+public interface IAnnounceManageService extends IBaseService<AnnounceManageEntity> {
+}
