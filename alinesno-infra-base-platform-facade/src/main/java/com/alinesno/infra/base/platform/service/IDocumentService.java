eec8ab343ad6d30523473ce30150467759a54c2e
王磊(Levi) <leviAip@gmail.com>
2025-02-21 01:09:40 +0800

    feat:新增 IDocumentService 接口

---
+++ alinesno-infra-base-platform-facade/src/main/java/com/alinesno/infra/base/platform/service/IDocumentService.java
@@ -0,0 +1,14 @@
+package com.alinesno.infra.base.platform.service;
+
+import com.alinesno.infra.base.platform.entity.AccountAuditEntity;
+import com.alinesno.infra.base.platform.entity.DocumentEntity;
+import com.alinesno.infra.common.facade.services.IBaseService;
+
+/**
+ *
+ *
+ * @author luoxiaodong
+ * @version 1.0.0
+ */
+public interface IDocumentService extends IBaseService<DocumentEntity> {
+}
