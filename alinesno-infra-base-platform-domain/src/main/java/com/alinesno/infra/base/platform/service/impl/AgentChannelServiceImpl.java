25ffb825c18e2a3c98dfae6fefdd2acaac09b57e
王磊(Levi) <leviAip@gmail.com>
2025-02-21 01:09:39 +0800

    feat:新增 AgentChannelServiceImpl 类，实现代理渠道服务接口

---
+++ alinesno-infra-base-platform-domain/src/main/java/com/alinesno/infra/base/platform/service/impl/AgentChannelServiceImpl.java
@@ -0,0 +1,14 @@
+package com.alinesno.infra.base.platform.service.impl;
+
+import com.alinesno.infra.base.platform.entity.AgentChannelEntity;
+import com.alinesno.infra.base.platform.mapper.AgentChannelMapper;
+import com.alinesno.infra.base.platform.service.IAgentChannelService;
+import com.alinesno.infra.common.core.service.impl.IBaseServiceImpl;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class AgentChannelServiceImpl extends IBaseServiceImpl<AgentChannelEntity, AgentChannelMapper> implements IAgentChannelService {
+
+}
