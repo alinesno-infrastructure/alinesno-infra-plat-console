904ed813a2a23cd9b60b82f438582fc676ca41df
王磊(Levi) <leviAip@gmail.com>
2025-02-21 01:09:42 +0800

    feat:新增 AgentRoleServiceImpl 类，实现代理角色服务

---
+++ alinesno-infra-base-platform-domain/src/main/java/com/alinesno/infra/base/platform/service/impl/AgentRoleServiceImpl.java
@@ -0,0 +1,14 @@
+package com.alinesno.infra.base.platform.service.impl;
+
+import com.alinesno.infra.base.platform.entity.AgentRoleEntity;
+import com.alinesno.infra.base.platform.mapper.AgentRoleMapper;
+import com.alinesno.infra.base.platform.service.IAgentRoleService;
+import com.alinesno.infra.common.core.service.impl.IBaseServiceImpl;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class AgentRoleServiceImpl extends IBaseServiceImpl<AgentRoleEntity, AgentRoleMapper> implements IAgentRoleService {
+
+}
