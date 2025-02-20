01246218f029460902a5168e3cbd0ded9c60da88
王磊(Levi) <leviAip@gmail.com>
2025-02-21 01:09:42 +0800

    feat: 实现 SolutionTypeServiceImpl 类，提供解决方案类型服务的基础实现。

---
+++ alinesno-infra-base-platform-domain/src/main/java/com/alinesno/infra/base/platform/service/impl/SolutionTypeServiceImpl.java
@@ -0,0 +1,14 @@
+package com.alinesno.infra.base.platform.service.impl;
+
+import com.alinesno.infra.base.platform.entity.SolutionTypeEntity;
+import com.alinesno.infra.base.platform.mapper.SolutionTypeMapper;
+import com.alinesno.infra.base.platform.service.ISolutionTypeService;
+import com.alinesno.infra.common.core.service.impl.IBaseServiceImpl;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class SolutionTypeServiceImpl extends IBaseServiceImpl<SolutionTypeEntity, SolutionTypeMapper> implements ISolutionTypeService {
+
+}
