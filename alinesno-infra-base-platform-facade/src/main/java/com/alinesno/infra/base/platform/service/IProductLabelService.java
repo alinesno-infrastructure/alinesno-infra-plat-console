d47f8f8da619cc17625aff14526a6f95e0702f7f
王磊(Levi) <leviAip@gmail.com>
2025-02-21 01:09:43 +0800

    feat:新增 IProductLabelService 接口

---
+++ alinesno-infra-base-platform-facade/src/main/java/com/alinesno/infra/base/platform/service/IProductLabelService.java
@@ -0,0 +1,14 @@
+package com.alinesno.infra.base.platform.service;
+
+import com.alinesno.infra.base.platform.entity.AccountAuditEntity;
+import com.alinesno.infra.base.platform.entity.ProductLabelEntity;
+import com.alinesno.infra.common.facade.services.IBaseService;
+
+/**
+ *
+ *
+ * @author luoxiaodong
+ * @version 1.0.0
+ */
+public interface IProductLabelService extends IBaseService<ProductLabelEntity> {
+}
