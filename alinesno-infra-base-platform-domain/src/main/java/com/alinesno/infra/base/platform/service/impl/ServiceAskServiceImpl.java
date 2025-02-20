8a5037ef3089d0d5b209ad76fc5fb6f9ca9fc320
王磊(Levi) <leviAip@gmail.com>
2025-02-21 01:09:42 +0800

    feat:新增 ServiceAskServiceImpl 类，实现服务接口

---
+++ alinesno-infra-base-platform-domain/src/main/java/com/alinesno/infra/base/platform/service/impl/ServiceAskServiceImpl.java
@@ -0,0 +1,14 @@
+package com.alinesno.infra.base.platform.service.impl;
+
+import com.alinesno.infra.base.platform.entity.ServiceAskEntity;
+import com.alinesno.infra.base.platform.mapper.ServiceAskMapper;
+import com.alinesno.infra.base.platform.service.IServiceAskService;
+import com.alinesno.infra.common.core.service.impl.IBaseServiceImpl;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class ServiceAskServiceImpl extends IBaseServiceImpl<ServiceAskEntity, ServiceAskMapper> implements IServiceAskService {
+
+}
