05f1e86f64b101dc6df2f43783c8b33e0f2e1247
王磊(Levi) <leviAip@gmail.com>
2025-02-21 01:09:39 +0800

    feat:新增 ProductTypeMapper 接口，定义持久层操作

---
+++ alinesno-infra-base-platform-domain/src/main/java/com/alinesno/infra/base/platform/mapper/ProductTypeMapper.java
@@ -0,0 +1,17 @@
+package com.alinesno.infra.base.platform.mapper;
+
+import com.alinesno.infra.base.platform.entity.AccountAuditEntity;
+import com.alinesno.infra.base.platform.entity.ProductTypeEntity;
+import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
+
+/**
+ * <p>
+ * 持久层接口
+ * </p>
+ *
+ * @author LuoXiaoDong
+ * @version 1.0.0
+ */
+public interface ProductTypeMapper extends IBaseMapper<ProductTypeEntity> {
+
+}
