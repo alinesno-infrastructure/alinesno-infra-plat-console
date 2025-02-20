b86c833d86a3403946c6940fb0382ac5441b9b83
王磊(Levi) <leviAip@gmail.com>
2025-02-21 01:09:41 +0800

    feat:新增 IAccountChannelService 接口

---
+++ alinesno-infra-base-platform-facade/src/main/java/com/alinesno/infra/base/platform/service/IAccountChannelService.java
@@ -0,0 +1,13 @@
+package com.alinesno.infra.base.platform.service;
+
+import com.alinesno.infra.base.platform.entity.AccountChannelEntity;
+import com.alinesno.infra.common.facade.services.IBaseService;
+
+/**
+ *
+ *
+ * @author luoxiaodong
+ * @version 1.0.0
+ */
+public interface IAccountChannelService extends IBaseService<AccountChannelEntity> {
+}
