ab9a2a892f9ee414644fad9e9faa53b7d450a5b2
李娜(Allen) <allenAip@gmail.com>
2025-05-01 00:32:06 +0800

    feat:新增 SubProduct 数据传输对象，包含基本属性定义。

---
+++ alinesno-infra-base-platform-console/src/main/java/com/alinesno/infra/base/platform/console/dto/SubProduct.java
@@ -0,0 +1,13 @@
+package com.alinesno.infra.base.platform.console.dto;
+
+import lombok.Data;
+
+@Data
+public class SubProduct {
+    private int hasInner;
+    private String linkPath;
+    private String prodStatus;
+    private String name;
+    private String id;
+    private String desc;
+}
